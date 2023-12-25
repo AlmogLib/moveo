f1a593f58ada7de5e6d22192197de65e2c5aa51b
AlmogLib <lieber.almog@gmail.com>
2023-12-25 22:38:42 +0200

    third commit

--- server/index.js
+++ server/index.js
@@ -18,13 +18,12 @@ const io = new Server(server, {
     },
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/moveo-task/client/public/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/public/index.html'));
 });
 
-// כל נתיב אחר מוביל לנתיב הראשי
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../client/public/index.html'));
+    res.sendFile(path.join(__dirname, 'client/public/index.html'));
 });
 
 // מערך לשמירת התפקידים של המשתמשים
