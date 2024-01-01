a72498ed349b333ea12846250eeb9b80835447c5
AlmogLib <lieber.almog@gmail.com>
2024-01-01 14:09:27 +0200

    fourth

--- server/index.js
+++ server/index.js
@@ -61,6 +61,10 @@ io.on("connection", (socket) => {
         io.emit("codeUpdated", { blockName, code });
     });
 
+    socket.on("resetUser", () => {
+        delete roles[socket.id];
+    });
+
 });
 
 server.listen(port, () => {
